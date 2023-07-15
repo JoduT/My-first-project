abe173de385770ca4cec142e86a268a7eccc70e2
JoduT <Sashakalachev73@gmail.com>
2023-07-15 16:32:10 +0300

    lesson5

--- adv_81/adv_81/settings.py
+++ adv_81/adv_81/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'adv_81.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
